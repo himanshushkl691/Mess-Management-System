456f46c700e939bb4e9115cbe42ab15f07b38155
sachin agrawal <slohiya406@gmail.com>
2019-10-27 02:31:20 +0530

    html pages created

---
+++ DBMS/dml.sql
@@ -0,0 +1,10 @@
+insert into users values("B170936CS","backchod","G");
+insert into users values("B170593CS","papaji","E");
+insert into users values("B170393CS","mittalbsdka","LH");
+insert into users values("B170703CS","shreyKpachpan","G");
+
+insert into extras values("B170936CS","1998-11-10","samosa",2,10,quantity*price);
+insert into extras values("B170936CS","1998-11-12","gulabjamun",2,15,quantity*price);
+insert into extras values("B170936CS","1998-11-13","Burger",1,30,quantity*price);
+insert into extras values("B170936CS","1998-11-13","guest",1,70,quantity*price);
+insert into extras values("B170593CS","1998-11-13","samosa",2,10,quantity*price);
